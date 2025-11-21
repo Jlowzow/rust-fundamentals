f459c56c1e690395826e0f3066d6761a607754c4
Joachim Lowzow <joachim@lowzow.org>
2025-11-21 21:20:54 +0200

    modified:   src/main.rs

--- examples/6-conditionals/conditionals/src/main.rs
+++ examples/6-conditionals/conditionals/src/main.rs
@@ -1,10 +1,16 @@
 // There are other conditionals that we can explore in Rust. Like using `if let`
 
 fn main() {
-    let maybe_number: Option<Option<()>> = None;
+    let maybe_number:Option<Option<Option<()>>> = Some(Some(None));
+    //let maybe_number: Option<Option<()>> = Some(None);
     //let maybe_number = Some(42);
     if let Some(number) = maybe_number {
         println!("The number is {:?}", number);
+        if let Some(number) = number {
+            println!("The value is {:?}", number);
+        } else {
+            println!("There is no value");
+        }
     } else {
         println!("There is no number");
     }
