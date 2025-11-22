bf99d94f38d8e52909da176e7c9354b161d9efe4
Joachim Lowzow <joachim@lowzow.org>
2025-11-22 21:30:59 +0200

    solved problem with map

--- examples/13-function-arguments/values/src/main.rs
+++ examples/13-function-arguments/values/src/main.rs
@@ -9,7 +9,18 @@ fn sum(numbers: &[i32]) -> i32 {
 
 fn main() {
     // There are no variadic arguments in Rust
-    let numbers = [1, 2, 3, 4, 5];
-    let result = sum(&numbers);
+
+    // take keyboard input to fill the array of arbitrary integers  
+    use std::io;
+    println!("Enter any number of integers separated by spaces:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let input_numbers: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    let result = sum(&input_numbers);
     println!("The sum is {}", result);
 }
